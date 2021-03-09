dc12bdcf8fdb3c5772abdd3c3f22f311c5c6857f
Dmitry Nugumanov <nugumanov6336@gmail.com>
2021-03-09 17:28:44 +0200

    Configure Project

---
+++ MyDevices/Source/TabBar/TabBarScreen.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDevices
+//
+//  Created by Dmitry Nugumanov on 09.03.2021.
+//
+
+import SwiftUI
+
+struct TabBarScreen: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarScreen()
+    }
+}
